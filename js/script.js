977ecfcf25c8ab26ce9a4685f07053f589bfc77b
Kacper <kacpergrzywacz2@gmail.com>
2025-10-28 22:20:21 +0100

    ref

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-const greatThan = (arr, callback) => {
+const modifyArr = (arr) => {
 	for (let i = 0; i < arr.length; i++) {
-		let index = arr[i];
-		if (index > 6) {
-			callback(index);
-		}
+		arr[i] *= 3;
 	}
 };
 
-const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-greatThan(tab, function (data) {
-	console.log("Data :", data);
-});
+const tab = [1, 2, 3, 4, 5, 6, 7];
+modifyArr(tab);
+console.log(tab);
