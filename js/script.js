7cecbf32acb584b02f21683cf98ef5d74c40e861
Kacper <kacpergrzywacz2@gmail.com>
2025-10-28 11:11:54 +0100

    modulo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-let array = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6];
-
 let sum = 0;
 
-for (let i = 0; i < array.length; i++) {
-	sum += array[i];
-	console.log(sum);
+for (let i = 0; i <= 14; i++) {
+	if (i % 2 == 0) {
+		sum += i;
+	}
 }
+console.log(sum);
