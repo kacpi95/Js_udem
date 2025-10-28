732fce6437c87591ee675d3f3b2ef6ebfb85814c
Kacper <kacpergrzywacz2@gmail.com>
2025-10-28 12:42:38 +0100

    func

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const multiplay = (a, b) => {
-	let sum = a * b;
+const averageValue = (a, b, c, d) => {
+	let sum = (a + b + c + d) / 4;
 	return sum;
 };
 
-console.log(multiplay(3, 10));
-console.log(multiplay(120, 56));
-console.log(multiplay(1234, 1234));
+console.log(averageValue(2, 4, 8, 10));
+console.log(averageValue(30, 60, 90, 120));
+console.log(averageValue(5, 10, 15, 20));
