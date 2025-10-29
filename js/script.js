6d91c2c843db3cdb4c676cf849d43ffc2274903b
Kacper <kacpergrzywacz2@gmail.com>
2025-10-29 13:42:39 +0100

    obj-metod-2-update

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ const school = {
 		const obj = {
 			name: name,
 			surname: surname,
+			grades: [],
 		};
-		const index = this.students.length;
-		this.students[index] = obj;
+
+		this.students.push(obj);
 	},
 	showStudents: function () {
 		if (this.students.length == 0) {
@@ -22,39 +23,74 @@ const school = {
 		}
 	},
 	showStudentsByName: function (name) {
-		if (this.students.length == 0) {
+		const filterStudents = this.students.filter((stud) => stud.name === name);
+
+		if (filterStudents.length == 0) {
 			console.log("Szkoła nie ma studentów ");
 			return;
 		}
 
-		for (let i = 0; i < this.students.length; i++) {
-			const stud = this.students[i];
-			if (stud.name === name) {
-				console.log(stud.name, stud.surname);
-			}
-		}
-		console.log("Imię studenta:", name);
+		console.log("Studenci o imieniu:", name, ":");
+
+		filterStudents.forEach((stud) => console.log(stud.name, stud.surname));
 	},
 	getNumStudents: function () {
-		let numStudents = this.students.length;
-		return numStudents;
+		return this.students.length;
 	},
 	resetStudents: function () {
 		this.students = [];
 	},
+	addGrade: function (name, surname, grade) {
+		const findStudent = this.students.find(
+			(stud) => stud.name === name && stud.surname === surname
+		);
+
+		if (!findStudent) {
+			console.log("Nie znaleziono studenta o podanym imieniu i nazwisku");
+			return;
+		}
+
+		findStudent.grades.push(grade);
+
+		console.log(`Dodano ocenę ${grade} dla ${name} ${surname}`);
+	},
+	showGrades: function (name, surname) {
+		const findStudent = this.students.find(
+			(stud) => stud.name === name && stud.surname === surname
+		);
+
+		if (!findStudent) {
+			console.log("Student nie ma żadnych ocen");
+			return;
+		}
+
+		console.log(
+			`Oceny studenta ${name} ${surname}`,
+			findStudent.grades
+		);
+	},
+	getAverageGrade: function (name, surname) {
+		const findStudent = this.students.find(
+			(stud) => stud.name === name && stud.surname === surname
+		);
+		if (findStudent.grades.length === 0) {
+			console.log("Student nie ma ocen");
+			return;
+		}
+
+		const sum = findStudent.grades.reduce((a, b) => a + b, 0);
+		const avg = sum / findStudent.grades.length;
+
+		return avg.toFixed(2);
+	},
 };
 
 school.addStudent("Kacper", "Kowalski");
-school.addStudent("Kacper", "Nowak");
 school.addStudent("Asia", "Nowak");
-school.addStudent("Ola", "Adamska");
-console.log(school.students);
-school.showStudents();
 
-school.showStudentsByName("Kacper");
+school.addGrade("Kacper", "Kowalski", 5);
+school.addGrade("Kacper", "Kowalski", 4);
+school.addGrade("Asia", "Nowak", 3);
 
-console.log(school.getNumStudents());
-
-school.resetStudents();
-
-console.log(school.getNumStudents());
+school.showGrades("Kacper", "Kowalski");
+console.log("Średnia:", school.getAverageGrade("Kacper", "Kowalski"));
