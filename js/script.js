2a57f53fcd49ac18c77c5d919aefc51a78435c50
Kacper <kacpergrzywacz2@gmail.com>
2025-10-29 22:22:48 +0100

    obj_func_constru-3

--- js/script.js
+++ js/script.js
@@ -1,28 +1,51 @@
-function Magazin(title) {
-	this.title = title;
-	this.adverts = [];
-
-	this.addAdvert = function (name, page) {
-		let obj = {
-			name: name,
-			page: page,
+function Library(name) {
+	this.name = name;
+	this.books = [];
+	this.addBook = function (title, author) {
+		const obj = {
+			title: title,
+			author: author,
+			isBorrowed: false,
 		};
-		this.adverts.push(obj);
+		this.books.push(obj);
 	};
-	this.printAdverts = function () {
-		for (let i = 0; i < this.adverts.length; i++) {
-			const mag = this.adverts[i];
+	this.showBooks = function () {
+		if (this.books.length === 0) {
+			console.log("Brak książek w bibliotece");
+		}
 
-			console.log(`Advert: ${mag.name}, ${mag.page}`);
+		for (let i = 0; i < this.books.length; i++) {
+			const book = this.books[i];
+
+			console.log(
+				`Title: ${book.title}, Author ${book.author}, Borrowed: ${book.isBorrowed}`
+			);
+		}
+	};
+	this.borrowBook = function (title) {
+		let findTitle = this.books.find((el) => el.title === title);
+
+		if (!findTitle) {
+			console.log(`Nie znaleziono ksiązki o tytule ${findTitle}`);
+			return;
+		}
+
+		if (findTitle.isBorrowed) {
+			console.log(`Książka jest już wypożyczona`);
+		} else {
+			findTitle.isBorrowed = true;
+			console.log(`Wypożyczono książkę: ${findTitle.title}`);
 		}
 	};
 }
 
-let magazine = new Magazin("WP");
-console.log(magazine);
+let lib = new Library("Biblioteka");
 
-magazine.addAdvert("laptop", 2);
-magazine.addAdvert("Console", 5);
-magazine.addAdvert("PC", 15);
+lib.addBook("Wiedźmin", "Andrzej Sapkowski");
+lib.addBook("Harry Potter", "J.K. Rowling");
+lib.addBook("Dziady", "Adam Mickiewicz");
 
-magazine.printAdverts();
+lib.showBooks();
+lib.borrowBook("Wiedźmin");
+lib.borrowBook("Wiedźmin");
+lib.showBooks();
