ae1278443bc97d32bcb4753b99621715a83cfc5c
Kacper <kacpergrzywacz2@gmail.com>
2025-10-30 12:07:17 +0100

    obj-gette-sette

--- js/script.js
+++ js/script.js
@@ -1,51 +1,20 @@
-function Library(name) {
-	this.name = name;
-	this.books = [];
-	this.addBook = function (title, author) {
-		const obj = {
-			title: title,
-			author: author,
-			isBorrowed: false,
-		};
-		this.books.push(obj);
-	};
-	this.showBooks = function () {
-		if (this.books.length === 0) {
-			console.log("Brak książek w bibliotece");
-		}
+const user = {
+	_name: null,
 
-		for (let i = 0; i < this.books.length; i++) {
-			const book = this.books[i];
-
-			console.log(
-				`Title: ${book.title}, Author ${book.author}, Borrowed: ${book.isBorrowed}`
-			);
-		}
-	};
-	this.borrowBook = function (title) {
-		let findTitle = this.books.find((el) => el.title === title);
-
-		if (!findTitle) {
-			console.log(`Nie znaleziono ksiązki o tytule ${findTitle}`);
-			return;
-		}
-
-		if (findTitle.isBorrowed) {
-			console.log(`Książka jest już wypożyczona`);
+	set name(newValue) {
+		if (newValue.length > 2) {
+			this._name = newValue;
 		} else {
-			findTitle.isBorrowed = true;
-			console.log(`Wypożyczono książkę: ${findTitle.title}`);
+			console.log("Name to short");
 		}
-	};
-}
+	},
 
-let lib = new Library("Biblioteka");
+	get name() {
+		return this._name;
+	},
+};
 
-lib.addBook("Wiedźmin", "Andrzej Sapkowski");
-lib.addBook("Harry Potter", "J.K. Rowling");
-lib.addBook("Dziady", "Adam Mickiewicz");
-
-lib.showBooks();
-lib.borrowBook("Wiedźmin");
-lib.borrowBook("Wiedźmin");
-lib.showBooks();
+user.name = "Kacper";
+console.log(user.name);
+user.name = "Adam";
+console.log(user.name);
