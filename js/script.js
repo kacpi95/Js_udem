78fde30351b386a8f331a61d70573258e508f7d5
Kacper <kacpergrzywacz2@gmail.com>
2025-10-30 23:52:26 +0100

    fil,map,som-2

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ console.log(new2);
 
 let new3 = numbers.filter((el) => el % 2 === 0);
 console.log(new3);
+
+const students = [
+	{ name: "Kacper", grade: 5 },
+	{ name: "Ola", grade: 2 },
+	{ name: "Asia", grade: 4 },
+];
+
+let new4 = students.filter((el) => {
+	return el.grade >= 3;
+});
+console.log(new4);
+
+let new5 = students.some((el) => el.grade === 5);
+console.log(new5);
+
+let new6 = students.every((el) => el.grade >= 3);
+console.log(new6);
+
+let new7 = students.find((el)=> el.name === 'Ola');
+console.log(new7);
