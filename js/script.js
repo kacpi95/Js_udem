e02ea74caa7b733df502365c75e8d748a70a7357
Kacper <kacpergrzywacz2@gmail.com>
2025-11-07 19:08:04 +0100

    class js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,22 @@
-const map = new Map();
-
-map.set("jabłko", 3.2);
-map.set("banan", 5);
-map.set("gruszka", 4);
-
-console.log(map.get("banan"));
-
-const map2 = new Map();
-
-map2.set(1, "kacper");
-map2.set(2, "adam");
-map2.set(3, "Kasia");
-
-if (map2.has(2) === true) {
-	console.log(`Użytkownik istnieje ${map2.get(2)}`);
-} else {
-	console.log("Użytkownik nie istnieje");
+class Book {
+	constructor(title, author, year) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+	}
+	getSummary() {
+		console.log(
+			`Tytuł: ${this.title}, Autor: ${this.author}, Rok: ${this.year}`
+		);
+	}
+	updateYear(newYear) {
+		this.year = newYear;
+	}
 }
 
-for (const [key, value] of map) console.log(key, value);
-
-const words = ["kot", "pies", "kot", "ryba", "pies", "kot"];
-
-const students = [
-	{ name: "Jan", grade: 5 },
-	{ name: "Anna", grade: 4 },
-	{ name: "Jan", grade: 3 },
-];
-
-const storeGrades = (students) => {
-	let map3 = new Map();
-
-	for (const student of students) {
-		const { name, grade } = student;
-		if (map3.has(name)) {
-			map3.get(name).push(grade);
-		} else {
-			map3.set(name, [grade]);
-		}
-	}
-	return map3;
-};
-
-console.log(storeGrades(students));
+const book1 = new Book("Harry Potter", "Rowling", 2005);
+const book2 = new Book("The Lord Of the Rings", "Tolkien", 2002);
+book1.getSummary();
+book2.getSummary();
+book2.updateYear(1992);
+book2.getSummary()
