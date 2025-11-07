ba3214c19c2cc8dcda1f0da57015af4402f7f509
Kacper <kacpergrzywacz2@gmail.com>
2025-11-07 19:29:51 +0100

    set, get in class

--- js/script.js
+++ js/script.js
@@ -19,4 +19,22 @@ const book2 = new Book("The Lord Of the Rings", "Tolkien", 2002);
 book1.getSummary();
 book2.getSummary();
 book2.updateYear(1992);
-book2.getSummary()
+book2.getSummary();
+
+class Person {
+	constructor(firstName, lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	set fullName(name) {
+		const fullName = name;
+		this.firstName = fullName;
+		this.lastName = fullName;
+	}
+	get fullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+}
+
+const person = new Person("Jan", "Kowalski");
+console.log(person.fullName);
